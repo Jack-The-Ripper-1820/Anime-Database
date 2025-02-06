aecf5e713a933a76d9ecae17e2d005f26183ad4f
Jack-The-Ripper-1820 <mayurjreddy@gmail.com>
2025-02-06 16:09:29 -0600

    home page route change and npm update

--- src/App.js
+++ src/App.js
@@ -15,22 +15,22 @@ function App() {
         <nav className="navbar">
           <ul className="nav-menu">
             <li className="nav-item">
-              <Link to="/Otaku-Database" className="nav-link">
+              <Link to="/Anime-Database" className="nav-link">
                 <h2 className="nav-title">Otaku Database</h2>
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/Otaku-Database/anime" className="nav-link">
+              <Link to="/Anime-Database/anime" className="nav-link">
                 <h3 className="nav-subtitle">Anime</h3>
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/Otaku-Database/manga" className="nav-link">
+              <Link to="/Anime-Database/manga" className="nav-link">
                 <h3 className="nav-subtitle">Manga</h3>
               </Link>
             </li>
             <li className="nav-item">
-              <Link to="/Otaku-Database/characters" className="nav-link">
+              <Link to="/Anime-Database/characters" className="nav-link">
                 <h3 className="nav-subtitle">Characters</h3>
               </Link>
             </li>
@@ -38,24 +38,24 @@ function App() {
         </nav>
 
         <Routes>
-          <Route path="/Otaku-Database/anime" element={<AnimeSearch />} />
-          <Route path="/Otaku-Database/manga" element={<MangaSearch />} />
-          <Route path="/Otaku-Database/characters" element={<CharacterSearch />} />
-          <Route path="/Otaku-Database" element={
+          <Route path="/Anime-Database/anime" element={<AnimeSearch />} />
+          <Route path="/Anime-Database/manga" element={<MangaSearch />} />
+          <Route path="/Anime-Database/characters" element={<CharacterSearch />} />
+          <Route path="/Anime-Database" element={
             <div className="home">
               <div className="home-content">
                 <h1 className="home-title">Welcome to The Otaku Database</h1>
                 <p className="home-subtitle">Place to find all the info on your favorite Anime, Manga and Characters.</p>
                 <div className="home-links">
-                  <Link to="/Otaku-Database/anime" className="home-link">
+                  <Link to="/Anime-Database/anime" className="home-link">
                     <img src={animeImage} alt="Anime" className="home-image" />
                     <h3 className="home-link-text">Anime</h3>
                   </Link>
-                  <Link to="/Otaku-Database/manga" className="home-link">
+                  <Link to="/Anime-Database/manga" className="home-link">
                     <img src={mangaImage} alt="Manga" className="home-image" />
                     <h3 className="home-link-text">Manga</h3>
                   </Link>
-                  <Link to="/Otaku-Database/characters" className="home-link">
+                  <Link to="/Anime-Database/characters" className="home-link">
                     <img src={charactersImage} alt="Characters" className="home-image" />
                     <h3 className="home-link-text">Characters</h3>
                   </Link>
